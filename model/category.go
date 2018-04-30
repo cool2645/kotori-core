e351799ac6893ca0507ab3d4b215a41234b39f67
LI JIAHAO <lijiahao99131@gmail.com>
2018-04-30 16:22:41 +0800

    finish cat and tag model
    Signed-off-by: LI JIAHAO <lijiahao99131@gmail.com>

--- model/category.go
+++ model/category.go
@@ -3,11 +3,14 @@ package model
 import (
 	"time"
 	"github.com/cool2645/kotori-core/config"
+	"github.com/jinzhu/gorm"
+	"github.com/pkg/errors"
 )
 
 type Category struct {
 	ID            uint       `gorm:"AUTO_INCREMENT" json:"id"`
-	Title         string     `gorm:"index" json:"title"`
+	Name          string     `gorm:"index" json:"name"`
+	Title         string     `json:"title"`
 	Description   string     `sql:"type:text;" json:"description"`
 	PublicPostNum uint       `gorm:"index" json:"public_post_num"`
 	PostNum       uint       `gorm:"index" json:"post_num"`
@@ -18,4 +21,80 @@ type Category struct {
 
 func (Category) TableName() string {
 	return config.GlobCfg.TablePrefix + "_categories"
+}
+
+var categoryPatchableFields = []string{
+	"name",
+	"title",
+	"description",
+}
+
+func ListCategories(db *gorm.DB, showAll bool, page uint, count int, orderBy string, order string) (categories []Category, total uint, err error) {
+	q := db.Order(orderBy + " " + order)
+	if !showAll {
+		q = q.Where("public_post_num > 0")
+	}
+	if count > 0 {
+		err = q.Limit(count).Offset((page - 1) * uint(count)).Find(&categories).Error
+	} else {
+		err = q.Find(&categories).Error
+	}
+	if err != nil {
+		err = errors.Wrap(err, "ListCategories")
+		return
+	}
+	err = db.Model(&Category{}).Count(&total).Error
+	if err != nil {
+		err = errors.Wrap(err, "ListCategories")
+		return
+	}
+	return
+}
+
+func GetCategoryByID(db *gorm.DB, id uint) (category Category, err error) {
+	err = db.Where("id = ?", id).First(&category).Error
+	if err != nil {
+		err = errors.Wrap(err, "GetCategoryByID")
+		return
+	}
+	return
+}
+
+func GetCategoryByName(db *gorm.DB, name string) (category Category, err error) {
+	err = db.Where("name = ?", name).First(&category).Error
+	if err != nil {
+		err = errors.Wrap(err, "GetCategoryByName")
+		return
+	}
+	return
+}
+
+func StoreCategory(db *gorm.DB, category *Category) (err error) {
+	if category.Name == "" {
+		category.Name = category.Title
+	}
+	err = db.Create(category).Error
+	if err != nil {
+		err = errors.Wrap(err, "StoreCategory")
+		return
+	}
+	return
+}
+
+func UpdateCategory(db *gorm.DB, category *Category, patch map[string]string) (err error) {
+	err = db.Model(category).Select(categoryPatchableFields).Updates(patch).Error
+	if err != nil {
+		err = errors.Wrap(err, "UpdateCategory")
+		return
+	}
+	return
+}
+
+func DeleteCategory(db *gorm.DB, id uint) (err error) {
+	err = db.Delete(Category{}, "id = ?", id).Error
+	if err != nil {
+		err = errors.Wrap(err, "DeleteCategory")
+		return
+	}
+	return
 }
--- model/tag.go
+++ model/tag.go
@@ -3,6 +3,8 @@ package model
 import (
 	"time"
 	"github.com/cool2645/kotori-core/config"
+	"github.com/jinzhu/gorm"
+	"github.com/pkg/errors"
 )
 
 type Tag struct {
@@ -17,4 +19,86 @@ type Tag struct {
 
 func (Tag) TableName() string {
 	return config.GlobCfg.TablePrefix + "_tags"
+}
+
+func ListTags(db *gorm.DB, showAll bool, since int, until int, page uint, count int, orderBy string, order string) (tags []Tag, total uint, err error) {
+	q := db.Order(orderBy + " " + order)
+	if !showAll {
+		q = q.Where("public_post_num > 0")
+	}
+	if since >= 0 {
+		q = q.Where("id > ?", since)
+	}
+	if until >= 0 {
+		q = q.Where("id < ?", until)
+	}
+	if count >= 0 {
+		err = q.Limit(count).Offset((page - 1) * uint(count)).Find(&tags).Error
+	} else {
+		err = q.Find(&tags).Error
+	}
+	if err != nil {
+		err = errors.Wrap(err, "ListTags")
+		return
+	}
+	err = db.Model(&Tag{}).Count(&total).Error
+	if err != nil {
+		err = errors.Wrap(err, "ListTags")
+		return
+	}
+	return
+}
+
+func GetTagByID(db *gorm.DB, id uint) (tag Tag, err error) {
+	err = db.Where("id = ?", id).First(&tag).Error
+	if err != nil {
+		err = errors.Wrap(err, "GetTagByID")
+		return
+	}
+	return
+}
+
+func GetTagByName(db *gorm.DB, name string) (tag Tag, err error) {
+	err = db.Where("name = ?", name).First(&tag).Error
+	if err != nil {
+		err = errors.Wrap(err, "GetTagByName")
+		return
+	}
+	return
+}
+
+func StoreTag(db *gorm.DB, tag *Tag) (err error) {
+	err = db.Create(tag).Error
+	if err != nil {
+		err = errors.Wrap(err, "StoreTag")
+		return
+	}
+	return
+}
+
+func CreateTag(db *gorm.DB, name string) (tag Tag, err error) {
+	tag, err = GetTagByName(db, name)
+	if err != nil && err.Error() == "GetTagByName: record not found" {
+		tag.Name = name
+		err = StoreTag(db, &tag)
+	}
+	return
+}
+
+func DeleteTagByID(db *gorm.DB, id uint) (err error) {
+	err = db.Delete(Tag{}, "id = ?", id).Error
+	if err != nil {
+		err = errors.Wrap(err, "DeleteTagByID")
+		return
+	}
+	return
+}
+
+func DeleteTagByName(db *gorm.DB, name string) (err error) {
+	err = db.Delete(Tag{}, "id = ?", name).Error
+	if err != nil {
+		err = errors.Wrap(err, "DeleteTagByName")
+		return
+	}
+	return
 }
